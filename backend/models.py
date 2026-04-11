04fe38af86f335e99881676f3b5b4f3452b30d71
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2026-04-11 23:40:12 +0530

    Create models.py

---
+++ backend/models.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, Integer, String, Float, Boolean
+from database import Base
+
+class Student(Base):
+    __tablename__ = "students"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    # Basic Info
+    name = Column(String, nullable=False)
+
+    # Academic
+    tenth = Column(Float)
+    twelfth = Column(Float)
+    be_cgpa = Column(Float)
+
+    # Skills & Projects
+    skills = Column(String)
+    domain = Column(String)
+    projects = Column(Integer)
+    hackathons = Column(Integer)
+    papers = Column(Integer)
+
+    # Placement
+    placed = Column(Boolean, default=False)
+    company = Column(String)
+    salary = Column(Float)
+    company_type = Column(String)  # Product / Service / Support
