607d1f142690076a877cc2f50471a580959cc793
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2026-04-19 12:46:44 +0530

    Update 1_Upload.py

--- frontend/pages/1_Upload.py
+++ frontend/pages/1_Upload.py
@@ -3,15 +3,13 @@ from utils import upload_csv
 
 st.title(" Upload Student Data")
 
-file = st.file_uploader("Upload CSV", type=["csv"])
-
+file = st.file_uploader("Upload CSV", type=["csv","xls"], accept_multiple_files =True)
+# st.file_uploader(label, type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, *, max_upload_size=None, disabled=False, label_visibility="visible", width="stretch")
 if file:
-    st.info("File ready")
-
+    st.info("File ready") ## st.info / st.write -- print
     if st.button("Upload & Store"):
         with st.spinner("Uploading..."):
             res = upload_csv(file.getvalue())
-
             if res.status_code == 200:
                 st.success("Uploaded successfully ✅")
                 st.json(res.json())
